4554d3c5ddeb468ff040fb6f6c7c0974b21fa962
Lyas <Egorlyasotsky@yandex.ru>
2023-04-16 17:27:31 +0400

    task_2 is ready

--- task_2/Program.cs
+++ task_2/Program.cs
@@ -3,3 +3,18 @@
 
 b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5) */
 
+Console.Clear();
+Console.Write("Введите координаты точек k1, b1, k2, b2: ");
+
+string[] num = Console.ReadLine().Split(" ");
+double k1 = int.Parse(num[0]);
+double b1 = int.Parse(num[1]);
+double k2 = int.Parse(num[2]);
+double b2 = int.Parse(num[3]);
+
+
+double x = ((b1 - b2) / (k1 - k2)) * -1;
+double y = k2 * x + b2;
+Console.WriteLine($"Точка перечечения прямых y={k1}*x+{b1} и y={k2}*x+{b2} равна ({x:f1}; {y:f1})");
+
+
